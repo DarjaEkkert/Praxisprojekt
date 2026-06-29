507ce67f5e2932aa2f680c07a5b9128fadb7eda2
Darja Ekkert <dara.ekkert@googlemail.com>
2026-06-29 16:10:28 +0200

    A2 RED: Test für Artikel hinzufügen

--- code/shoppingcart/ShoppingCartTest.java
+++ code/shoppingcart/ShoppingCartTest.java
@@ -6,11 +6,21 @@ import org.junit.jupiter.api.Test;
 
 public class ShoppingCartTest {
 
+@Test
+void neuerWarenkorbIstLeer() {
+
+    ShoppingCart cart = new ShoppingCart();
+
+    assertEquals(0, cart.getItemCount());
+}
+
     @Test
-    void neuerWarenkorbIstLeer() {
+void artikelKannHinzugefuegtWerden() {
 
-        ShoppingCart cart = new ShoppingCart();
+    ShoppingCart cart = new ShoppingCart();
 
-        assertEquals(0, cart.getItemCount());
-    }
+    cart.addItem("Buch");
+
+    assertEquals(1, cart.getItemCount());
+}
 }
